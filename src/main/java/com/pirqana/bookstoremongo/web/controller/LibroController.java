f7541bb8f621254acd614672e9bea7c722573e3c
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-04-12 00:24:01 -0500

    Config Cache

--- src/main/java/com/pirqana/bookstoremongo/web/controller/LibroController.java
+++ src/main/java/com/pirqana/bookstoremongo/web/controller/LibroController.java
@@ -2,9 +2,11 @@ package com.pirqana.bookstoremongo.web.controller;
 
 import com.pirqana.bookstoremongo.application.dto.libro.LibroDto;
 import com.pirqana.bookstoremongo.application.service.LibroService;
+import com.pirqana.bookstoremongo.web.config.DBCacheConfig;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +26,7 @@ public class LibroController {
     @ApiResponse(responseCode = "200")
     @ApiResponse(responseCode = "400", description = "Invalid id", content = @Content)
     @ApiResponse(responseCode = "404", description = "Not found", content = @Content)
+    @Cacheable(value = DBCacheConfig.CACHE_NAME)
     public ResponseEntity<List<LibroDto>> getAll() {
         return ResponseEntity.ok(libroService.findAll());
     }
